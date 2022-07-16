fe402acd104d44237d0eef35a432d757239b4316
Trai Torti <trai4.t@gmail.com>
2022-07-16 16:08:15 +1000

    Added in a base ordering process frontend

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import json
 
+#Global Variable
+jsonFile = open('data.json')
+data = json.load(jsonFile)
+orderList = [x for x in data["orders"]]
+orderIdList = [str(x["orderId"]) for x in orderList]
+
 def main():
-    pass
+    '''Initiate the script'''
+
+    print(orderIdList)
+    print('Please select which orders you would like to run, example input: 1122, 1123, 1124')
+    orderRunQuestion = input()
+    selectedId = list(orderRunQuestion.split(', '))
+    
+    if selectedId is not None and any(x in selectedId for x in orderIdList):
+        processOrders(selectedId)
+    else:
+        print('No orders are to be run, thank you for using NOMSS order run tool.')
 
 def processOrders():
     pass
