c087dca0f208f209beaf0d1e7ccbd0793c486dcc
Trai Torti <trai4.t@gmail.com>
2022-07-16 16:09:55 +1000

    Started bulk of the work
    Added variables and made each product quantity hold each product quantity value.

--- main.py
+++ main.py
@@ -20,7 +20,33 @@ def main():
         print('No orders are to be run, thank you for using NOMSS order run tool.')
 
 def processOrders():
-    pass
+    '''Bulk of the order processing work'''
+
+    #processOrders variables
+    productList = [x for x in data["products"]]
+    allItemsList = [x["items"] for x in orderList]
+    unfulfilledIdList = []
+    productInfo1 = []
+    productInfo2 = []
+    productInfo3 = []
+    productQuant1 = 0
+    productQuant2 = 0
+    productQuant3 = 0
+    orderStatus = ''
+
+    #Assigning each product quantity to a variable
+    for info in productList:
+        productId = str(info["productId"])
+        for id in productId:
+            if '1' in id:
+                productInfo1 = info
+                productQuant1 = int(productInfo1["quantityOnHand"])
+            elif '2' in id:
+                productInfo2 = info
+                productQuant2 = int(productInfo2["quantityOnHand"])
+            else:
+                productInfo3 = info
+                productQuant3 = int(productInfo3["quantityOnHand"])
 
 def reOrders():
     pass
